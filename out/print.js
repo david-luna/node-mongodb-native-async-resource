e927903f83f3e758ad93f9b23ee23477541eab68
David Luna <luna.bistuer@gmail.com>
2023-09-13 18:42:59 +0200

    feat: add elastic format into print script

--- out/print.js
+++ out/print.js
@@ -25,53 +25,95 @@
 const { existsSync, readFileSync } = require('fs');
 const path = require('path');
 
-// XXX
-const OTEL_FILE = path.resolve(path.join(__dirname, './elastic.log'));
+const fileName = process.argv[2]; // must be 'elastic' or 'otel'
 
-if (existsSync(OTEL_FILE)) {
-  const fileContent = readFileSync(OTEL_FILE, { encoding: 'utf-8' });
-  const fileLines = fileContent.split('\n').filter(l => l);
-  const quotedContent = fileLines.map(quoteProps).join('\n');
-  const spanList = JSON.parse(`[${quotedContent}]`);
-  const children = {};
-  const parents = [];
+if (fileName !== 'elastic' && fileName !== 'otel') {
+  console.error(`APM name invalid (${fileName}). Accepted values are elastic or otel.`)
+  process.exit(-1);
+}
 
-  for (const span of spanList) {
-    if (span.parentId) {
-      children[span.parentId] = children[span.parentId] || [];
-      children[span.parentId].push(span);
-    } else {
-      parents.push(span);
-    }
+const LOG_PATH = path.resolve(path.join(__dirname, `./${fileName}.log`));
+
+if (!existsSync(LOG_PATH)) {
+  console.warn(`There is no log file to process. Start the server and send some requests 1st`);
+  process.exit(0);
+}
+
+const fileContent = readFileSync(LOG_PATH, { encoding: 'utf-8' });
+const spanList = getObjects(fileContent);
+const parentSpans = [];
+const childSpans = {};
+
+for (const span of spanList) {
+  const parentId = span.parentId || span.parent_id;
+  if (parentId) {
+    childSpans[parentId] = childSpans[parentId] || [];
+    childSpans[parentId].push(span);
+  } else {
+    parentSpans.push(span);
   }
+}
 
-  for (const p of parents) {
-    // TODO: accept either OTel or Elastic property names.
-    console.log(`trace ${p.traceId.substring(6)}`);
-    console.log(`\`- span ${p.id.substring(6)} "${p.name}" (${p.attributes['http.url']} -> ${p.attributes['http.status_code']})`);
-    if (children[p.id]) {
-      for (const c of children[p.id]) {
-        console.log(`   \`- span ${c.id.substring(6)} "${c.name}" (${c.attributes['db.system']})`);
-      }
-    }
+for (const parent of parentSpans) {
+  const traceId = (parent.traceId || parent.trace_id).substring(6);
+  console.log(`trace ${traceId}`);
+  printSpan(parent);
+  
+  const children = childSpans[parent.id] || [];
+  for (const child of children) {
+    printSpan(child, '   ');
+  }
+}
+
+function printSpan(span, indent = '') {
+  const isOtelSpan = !!span.kind;
+  const isHttpSpan = span.kind === 1 || span.type === 'request';
+  const isDbSpan = span.kind === 2 || span.type === 'db';
+  const spanId = span.id.substring(6);
+  const spanName = span.name;
+
+  if (isHttpSpan) {
+    const spanUrl = isOtelSpan ? span.attributes['http.url'] : span.context.request.url.full;
+    const spanStatus = isOtelSpan ? span.attributes['http.status_code'] : span.context.response.status_code;
+
+    console.log(`${indent}\`- span ${spanId} "${spanName}" (${spanUrl} -> ${spanStatus})`);
+  } else if (isDbSpan) {
+    const dbSystem = isOtelSpan ? span.attributes['db.system'] : span.subtype;
+
+    console.log(`${indent}\`- span ${spanId} "${spanName}" (${dbSystem})`);
   }
 }
 
 
-function quoteProps(str, index, arr) {
-  if (index === arr.length - 1) {
-    return str;
-  }
-  if (str === '}') {
-    return '},';
-  }
-  if (str.indexOf('undefined,') !== -1) {
-    return '';
-  }
+function getObjects(content) {
+  const lines = content.split('\n');
+  const spans = [];
+  let text = '';
 
-  let res = str.replace(/"/g, '\\"');
-  res = res.replace(/'/g, '"');
-  res = res.replace(/([\w_]+): /g, (m, g) => `"${g}":`);
-
-  return res;
+  for (const line of lines) {
+    if (line === '{') {
+      try {
+        spans.push(JSON.parse(text))
+      } catch (err) {
+        // We may end here if there is a previous log like
+        // `Server listening on port 3000`
+        // before the console exporters start printing
+        // console.error(err);
+      }
+      text = '{';
+      continue;
+    }
+    
+    // Only treat defined properties
+    if (line.indexOf(': undefined') === -1) {
+      // ecape double quotes
+      let res = line.replace(/"/g, '\\"');
+      // single quote to double
+      res = res.replace(/'/g, '"');
+      // properties must be quoted
+      res = res.replace(/([\w_]+): /g, (m, g) => `"${g}":`);
+      text += res;
+    }
+  }
+  return spans;
 }
