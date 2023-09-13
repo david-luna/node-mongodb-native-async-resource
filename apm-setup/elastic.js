09042c5042881168b9f8bcb215eabe979f07d32d
Trent Mick <trentm@gmail.com>
2023-09-13 15:08:41 -0700

    typos

--- apm-setup/elastic.js
+++ apm-setup/elastic.js
@@ -2,7 +2,6 @@ const apm = require('elastic-apm-node').start({
   serviceName: 'cats-service',
   metricsInterval: '0s',
   logLevel: 'off',
-  apiRequestTime: '3s', // XXX for local usage of mockapmserver.js
   disableSend: true, // for the ones not having a mock APM server
 });
 
